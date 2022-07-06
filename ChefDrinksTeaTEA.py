9b78185ce645e754c394369c63ba2610cfa05067
Younis <younisrahman007@gmail.com>
2022-07-07 02:33:44 +0600

    tea

--- ChefDrinksTeaTEA.py
+++ ChefDrinksTeaTEA.py
@@ -1,11 +1,13 @@
 n = int(input())
-count = 1
-loop = False
 for i in range(n):
     x, y, z = map(int, input().split())
-    loop = True
-    while loop:
-        if (y*count) <= x:
-            print(count*z)
-            loop = False
-        count += 1
+    if x <= y:
+        print(z)
+    else:
+        loop = True
+        count = 1
+        while loop:
+            if (y*count) >= x:
+                print(count*z)
+                loop = False
+            count += 1
