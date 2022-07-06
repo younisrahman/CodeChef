95541a0caf136921d544a5049402632a411f4148
Younis <younisrahman007@gmail.com>
2022-07-07 02:06:49 +0600

    High Accuracy

---
+++ HighAccuracyACCURACY.py
@@ -0,0 +1,15 @@
+n = int(input())
+count = 0
+loop = False
+for i in range(n):
+    num = int(input())
+    if num % 3 == 0:
+        print(0)
+    else:
+        loop = True
+        count = 1
+        while loop:
+            if num+count % 3 == 0:
+                print(count)
+                loop = False
+            count += 1
