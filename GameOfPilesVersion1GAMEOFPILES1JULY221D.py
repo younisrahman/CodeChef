d3dbd7dc2b9ab6c571521d584f4dd59157601dbe
Younis <younisrahman007@gmail.com>
2022-07-09 01:04:37 +0600

    GameOfPilesVersion2GAMEOFPILES2JULY221D

--- GameOfPilesVersion1GAMEOFPILES1JULY221D.py
+++ GameOfPilesVersion1GAMEOFPILES1JULY221D.py
@@ -1,28 +1,2 @@
 
 for i in range(int(input())):
-    # for j in range(int(input())):
-    n = int(input())
-    x = list(map(int, input().split()))
-    x = sorted(x)
-    turn = 'CHEF'
-    if x[0] == 1:
-        print("CHEF")
-    elif len(x) == 1:
-        if x[0] % 2 == 0:
-            print("CHEFINA")
-        else:
-            print("CHEF")
-    else:
-        for k in range(n):
-            print('k', k)
-            for l in range(x[k]-1):
-                print('l', l)
-                if turn == 'CHEF':
-                    turn = 'CHEFINA'
-                else:
-                    turn = 'CHEF'
-
-        if turn == 'CHEF':
-            print('CHEFINA')
-        else:
-            print('CHEF')
