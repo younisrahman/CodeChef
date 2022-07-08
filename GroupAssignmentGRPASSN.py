e894056c914ec27a5e351383efeee81a335bc7c9
Younis <younisrahman007@gmail.com>
2022-07-08 14:09:56 +0600

    Group Assignment

--- GroupAssignmentGRPASSN.py
+++ GroupAssignmentGRPASSN.py
@@ -1,1 +1,21 @@
 # Group Assignment
+
+for i in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    dict = {}
+    for j in a:
+        if j in dict:
+            dict[j] += 1
+        else:
+            dict[j] = 1
+    list_keys = list(dict.keys())
+    list_values = list(dict.values())
+    count = 0
+    for k in range(len(list_keys)):
+        if list_values[k] % list_keys[k] != 0:
+            print("NO")
+            count += 1
+            break
+    if count == 0:
+        print("YES")
