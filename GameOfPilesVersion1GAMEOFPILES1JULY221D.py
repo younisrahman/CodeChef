5134106f687c973cb8e4d1074585a9a66e7623b6
Younis <younisrahman007@gmail.com>
2022-07-09 01:11:45 +0600

    GameOfPilesVersion2GAMEOFPILES2JULY221D

--- GameOfPilesVersion1GAMEOFPILES1JULY221D.py
+++ GameOfPilesVersion1GAMEOFPILES1JULY221D.py
@@ -1,2 +1,17 @@
 
 for i in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))[:n]
+    s = 0
+    c = 0
+    for i in range(n):
+        s += arr[i]
+        if arr[i] == 1:
+            c += 1
+    if c > 0:
+        print("CHEF")
+    else:
+        if s % 2 == 0:
+            print("CHEFINA")
+        else:
+            print("CHEF")
--- GameOfPilesVersion2GAMEOFPILES2JULY221D.py
+++ GameOfPilesVersion2GAMEOFPILES2JULY221D.py
@@ -1,25 +1,21 @@
-turn = 'CHEF'
-for i in range(int(input())):
-    # for j in range(int(input())):
-    n = int(input())
-    x = list(map(int, input().split()))
-    s = sum(x)
-    if s % 2 == 0:
-        print("CHEFINA")
-    else:
-        print("CHEF")
 
-    # if s % 2 == 0:
-    #     if turn == 'CHEF':
-    #         turn = 'CHEFINA'
-    #         print('CHEF')
-    #     else:
-    #         turn = 'CHEF'
-    #         print('CHEFINA')
-    # else:
-    #     if turn == 'CHEF':
-    #         turn = 'CHEFINA'
-    #         print('CHEF')
-    #     else:
-    #         turn = 'CHEF'
-    #         print('CHEFINA')
+for i in range(int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))[:n]
+    s = 0
+    c = 0
+    for i in range(n):
+        s += arr[i]
+        if arr[i] == 1:
+            c += 1
+
+    if c > 0:
+        if c % 2 == 0:
+            print("CHEFINA")
+        else:
+            print("CHEF")
+    else:
+        if s % 2 == 0:
+            print("CHEFINA")
+        else:
+            print("CHEF")
