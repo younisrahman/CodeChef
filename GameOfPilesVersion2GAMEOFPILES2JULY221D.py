6845294d74e56e2122baf6a90927e90bf9712a1b
Younis <younisrahman007@gmail.com>
2022-07-08 22:07:43 +0600

    GameOfPilesVersion2GAMEOFPILES2JULY221D

--- GameOfPilesVersion2GAMEOFPILES2JULY221D.py
+++ GameOfPilesVersion2GAMEOFPILES2JULY221D.py
@@ -4,43 +4,22 @@ for i in range(int(input())):
     n = int(input())
     x = list(map(int, input().split()))
     s = sum(x)
-    # if s == 0:
-    #     if turn == 'CHEF':
-    #         turn = 'CHEFINA'
-    #         print('CHEF')
-    #     else:
-    #         turn = 'CHEF'
-    #         print('CHEFINA')
-    # elif s == 1:
-    #     if turn == 'CHEF':
-    #         turn = 'CHEFINA'
-    #         print('CHEF')
-    #     else:
-    #         turn = 'CHEF'
-    #         print('CHEFINA')
-    # else:
     if s % 2 == 0:
-        if turn == 'CHEF':
-            turn = 'CHEFINA'
-            print('CHEF')
-        else:
-            turn = 'CHEF'
-            print('CHEFINA')
+        print("CHEFINA")
     else:
-        if turn == 'CHEF':
-            turn = 'CHEFINA'
-            print('CHEF')
-        else:
-            turn = 'CHEF'
-            print('CHEFINA')
-    # if s % 2 == 0:
-    #     if turn == 0:
-    #         print("CHEF")
-    #     else:
-    #         print("CHEFINA")
+        print("CHEF")
 
-    # else:
-    #     if turn == 0:
-    #         print("CHEF")
+    # if s % 2 == 0:
+    #     if turn == 'CHEF':
+    #         turn = 'CHEFINA'
+    #         print('CHEF')
     #     else:
-    #         print("CHEFINA")
+    #         turn = 'CHEF'
+    #         print('CHEFINA')
+    # else:
+    #     if turn == 'CHEF':
+    #         turn = 'CHEFINA'
+    #         print('CHEF')
+    #     else:
+    #         turn = 'CHEF'
+    #         print('CHEFINA')
