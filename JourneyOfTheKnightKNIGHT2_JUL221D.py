9adb4b1942a488f4626830fb180d66069f6086c7
Younis <younisrahman007@gmail.com>
2022-07-09 17:00:32 +0600

    Knight accept

--- JourneyOfTheKnightKNIGHT2_JUL221D.py
+++ JourneyOfTheKnightKNIGHT2_JUL221D.py
@@ -29,44 +29,10 @@ for i in range(int(input())):
     max = 8
     total = 0
     x, y, x2, y2 = map(int, input().split())
-    # print(knight(x, y, x2, y2, max, total))
-    while total <= 100:
-        print(total)
-        if x == x2 and y == y2:
-            print(x, y, total)
-            break
-        if x+1 <= max and y+2 <= max:
-            x = x+1
-            y = y+2
-            total = total+1
-        elif x+2 <= max and y+1 <= max:
-            x = x+2
-            y = y+1
-            total = total+1
-        elif x-1 >= 0 and y+2 <= max:
-            x = x-1
-            y = y+2
-            total = total+1
-        elif x-2 >= 0 and y+1 <= max:
-            x = x-2
-            y = y+1
-            total = total+1
-        elif x-2 >= 0 and y-1 >= 0:
-            x = x-2
-            y = y-1
-            total = total+1
-        elif x-1 >= 0 and y-2 >= 0:
-            x = x-1
-            y = y-2
-            total = total+1
-        elif x+1 <= max and y-2 >= 0:
-            x = x+1
-            y = y-2
-            total = total+1
-        elif x+2 <= max and y-1 >= 0:
-            x = x+2
-            y = y-1
-            total = total+1
-        else:
-            total+1
-    print(x, y, total)
+
+    if (x+y) % 2 == 0 and (x2+y2) % 2 == 0:
+        print("YES")
+    elif (x+y) % 2 == 1 and (x2+y2) % 2 == 1:
+        print("YES")
+    else:
+        print("NO")
