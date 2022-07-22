773d76b2f8b786af4d0835681c6367e27f840045
Younis <younisrahman007@gmail.com>
2022-07-23 01:01:33 +0600

    ToDivideorNotToDivideDIVAB_JULY222D

--- ToDivideorNotToDivideDIVAB_JULY222D.py
+++ ToDivideorNotToDivideDIVAB_JULY222D.py
@@ -1,10 +1,38 @@
-# ToDivideorNotToDivideDIVAB_JULY222D
-for i in (range(int(input()))):
-    a, b, n = map(int, input().split())
-    if a % b == 0:
-        print('-1')
-    else:
-        if (((n//a)+1)*a) % b == 0:
-            print(((n//a)+2)*a)
-        else:
-            print(((n//a)+1)*a)
+# # ToDivideorNotToDivideDIVAB_JULY222D
+# for i in (range(int(input()))):
+#     a, b, n = map(int, input().split())
+#     if a % b == 0:
+#         print('-1')
+#     else:
+#         if (((n//a)+1)*a) % b == 0:
+#             print(((n//a)+2)*a)
+#         else:
+#             print(((n//a)+1)*a)
+
+# # ToDivideorNotToDivideDIVAB_JULY222D
+# for i in (range(int(input()))):
+#     a, b, n = map(int, input().split())
+#     if a % b == 0:
+#         print('-1')
+#     else:
+#         x = n
+#         if not(x % a == 0):
+#             x = (n+a)-(x % a)
+#         else:
+#             while not ((x % a == 0) and not(x % b == 0)):
+#                 x += a
+#         print(x)
+
+
+# for i in (range(int(input()))):
+#     a, b, n = map(int, input().split())
+#     if a % b == 0:
+#         print('-1')
+#     else:
+#         x = n
+#         if not(x % a == 0):
+#             x = (n+a)-(x % a)
+#         else:
+#             while not ((x % a == 0) and not(x % b == 0)):
+#                 x += a
+#         print(x)
