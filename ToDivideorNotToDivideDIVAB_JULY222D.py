901938945e01fea8c1d503fd7bb11b0bd265166f
Younis <younisrahman007@gmail.com>
2022-07-23 00:49:43 +0600

    ToDivideorNotToDivideDIVAB_JULY222D

--- ToDivideorNotToDivideDIVAB_JULY222D.py
+++ ToDivideorNotToDivideDIVAB_JULY222D.py
@@ -1,1 +1,10 @@
 # ToDivideorNotToDivideDIVAB_JULY222D
+for i in (range(int(input()))):
+    a, b, n = map(int, input().split())
+    if a % b == 0:
+        print('-1')
+    else:
+        if (((n//a)+1)*a) % b == 0:
+            print(((n//a)+2)*a)
+        else:
+            print(((n//a)+1)*a)
