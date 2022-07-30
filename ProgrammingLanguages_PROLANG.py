0826cec190b96abc60ef8376fe2f0e7313cb4d6c
Younis <younisrahman007@gmail.com>
2022-07-30 17:40:56 +0600

    ProgrammingLanguages_PROLANG

---
+++ ProgrammingLanguages_PROLANG.py
@@ -0,0 +1,28 @@
+for i in range(int(input())):
+    arr = list(map(int, input().split()))
+    one = 0
+    two = 0
+    if arr[0] == arr[2]:
+        one += 1
+    elif arr[0] == arr[3]:
+        one += 1
+    if arr[1] == arr[2]:
+        one += 1
+    elif arr[1] == arr[3]:
+        one += 1
+
+    if arr[0] == arr[4]:
+        one += 1
+    elif arr[0] == arr[5]:
+        one += 1
+    if arr[1] == arr[4]:
+        one += 1
+    elif arr[1] == arr[5]:
+        one += 1
+
+    if one == 2:
+        print(1)
+    elif two == 2:
+        print(2)
+    else:
+        print(0)
