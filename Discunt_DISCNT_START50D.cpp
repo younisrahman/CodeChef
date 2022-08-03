ec88756435395c6747dce0500e9c3fa66b1f13a3
Younis <younisrahman007@gmail.com>
2022-08-03 21:41:59 +0600

    Discunt_DISCNT_START50D

---
+++ Discunt_DISCNT_START50D.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int t, x;
+    cin >> t;
+    while (t--)
+    {
+        cin >> x;
+        cout << (100 - x) << endl;
+    }
+
+    return 0;
+}
