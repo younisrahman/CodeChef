c7a829f6f1d32a7e3f8d60172280a19a2e6b9b77
Younis <younisrahman007@gmail.com>
2022-08-04 13:27:54 +0600

    A or B_STARTS39.cpp

---
+++ AveragePermutation_AVGPERM_STAR50D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int marvel()
+{
+    int n;
+    cin >> n;
+    if (n == 3)
+    {
+        cout << 1 << " " << 2 << " " << 3 << endl;
+        return 0;
+    }
+    cout << n << " " << n - 2 << " ";
+    for (int i = 1; i < n - 3; i++)
+    {
+        cout << i << " ";
+    }
+    cout << n - 3 << " " << n - 1 << endl;
+    return 0;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        marvel();
+    }
+}
