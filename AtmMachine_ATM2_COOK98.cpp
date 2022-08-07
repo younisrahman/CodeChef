cf63f7010131f7d036bfac30ad04dbeca9e04a71
Younis <younisrahman007@gmail.com>
2022-08-07 16:25:09 +0600

    AtmMachine_ATM2_COOK98.cpp

--- AtmMachine_ATM2_COOK98.cpp
+++ AtmMachine_ATM2_COOK98.cpp
@@ -4,12 +4,30 @@ using namespace std;
 int main()
 {
     int n, a, b, c, d;
-    vector<int> vector;
-    set<int> set;
+
     cin >> n;
     while (n--)
     {
-        cin >> a >> b >> c >> d;
+        vector<int> vector;
+        cin >> a >> b;
+        for (int i = 0; i < a; i++)
+        {
+            cin >> c;
+            if (b - c >= 0)
+            {
+                b -= c;
+                vector.push_back(1);
+            }
+            else
+            {
+                vector.push_back(0);
+            }
+        }
+        for (int i = 0; i < vector.size(); i++)
+        {
+            cout << vector[i];
+        }
+        cout << endl;
     }
     return 0;
 }
