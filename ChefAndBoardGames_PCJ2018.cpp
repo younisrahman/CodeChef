4cfc874531adc02a839601abbc0cd25bbb39c123
Younis <younisrahman007@gmail.com>
2022-08-07 17:54:02 +0600

    ChefAndBoardGames_PCJ2018

--- ChefAndBoardGames_PCJ2018.cpp
+++ ChefAndBoardGames_PCJ2018.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int main()
+{
+    int n, a, b, c, d;
+    vector<int> vector;
+    set<int> set;
+    cin >> n;
+    while (n--)
+    {
+        cin >> a;
+        int ans = 0;
+        for (int i = 1; i <= a;)
+        {
+            ans += (a - i + 1) * (a - i + 1);
 
+            i += 2;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
