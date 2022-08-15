66dc8aca19d67e447deafc0660f3cf6a667a2817
Younis Rahman <younisrahman007@gmail.com>
2022-08-15 18:41:24 +0600

    avg

--- AverageNumber_LTIME69.cpp
+++ AverageNumber_LTIME69.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 int main()
 {
-int n,a,b,c,d;
-vector<int>vector;
-set<int> set;
-cin>>n;
-while(n--)
-{
-cin>>a>>b>>c>>d;
-}
+    int n, a, b, c, d, e, f;
+    cin >> n;
+    while (n--)
+    {
+
+        cin >> a >> b >> c;
+        cin >> d >> e >> f;
+        int sum = d + e + f, total = (a + b) * c, restOfAmount = total - sum;
+        if (restOfAmount % b == 0 && restOfAmount>= b)
+            cout << restOfAmount / b << endl;
+        else
+            cout << -1 << endl;
+    }
     return 0;
 }
